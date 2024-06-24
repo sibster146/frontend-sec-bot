35dd87368168eee4734680c54aae809d2c981290
Siby Suriyan <110351118+sibster146@users.noreply.github.com>
2024-06-24 14:17:53 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -5,7 +5,7 @@ import urllib.parse
 
 st.title("Security Bot (RAG)")
 
-if "message" not in st.session_state:
+if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
@@ -13,24 +13,23 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role":"user","content": prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     base_url = "https://ztwheo6lrfirvfzlkyuvliu7zy0eibve.lambda-url.us-east-1.on.aws/"
     params = {
-        'query':prompt
+        "query": prompt
     }
     encoded_params = urllib.parse.urlencode(params)
     full_url = f"{base_url}?{encoded_params}"
     with st.chat_message("assistant"):
-        stream = requests.get(full_url, stream=True)
+        stream = requests.get(full_url,stream=True)
         complete_text_list = []
-        for chunk in stream.iter_content(chunk_size=1024):
+        for chunk in stream.iter_content(chunk_size = 1024):
             if chunk:
                 text = chunk.decode('utf-8').replace("\\n","\n").replace("\\'","'")
                 st.markdown(text)
                 complete_text_list.append(text)
     complete_text_string = "".join(complete_text_list)
-    st.session_state.messages.append({"role":"assistant","content":complete_text_string})
-
+    st.session_state.messages.append({"role": "assistant", "content": complete_text_string})
