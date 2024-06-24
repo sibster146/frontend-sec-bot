a8183e15594e06cd4769a2619543cf4dd4359071
Siby Suriyan <110351118+sibster146@users.noreply.github.com>
2024-06-24 14:05:23 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -13,10 +13,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({
-        "role":"user",
-        "content": prompt
-    })
+    st.session_state.messages.append({"role":"user","content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
@@ -27,7 +24,13 @@ if prompt := st.chat_input("What is up?"):
     encoded_params = urllib.parse.urlencode(params)
     full_url = f"{base_url}?{encoded_params}"
     with st.chat_message("assistant"):
-        stream = requests.get(full_url)
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role":"assistant","content":response})
+        stream = requests.get(full_url, stream=True)
+        complete_response_list = []
+        for chunk in stream.iter_content(chunk_size=1024):
+            if chunk:
+                response_text = chunk.decode('utf-8').replace("\\n","\n").replace("\\'","'")
+                st.markdown(response_text)
+                complete_response_list.append(response_text)
+    complete_response_string = "".join(complete_response_list)
+    st.session_state.messages.append({"role":"assistant","content":complete_response_string})
 
